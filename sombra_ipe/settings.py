642d3d03c71ed38fc4cbf8b730e29daedd78c5b5
Daniela Baliza <daniinhasinnha@gmail.com>
2026-03-22 17:05:38 -0300

    teste allowed hoss

--- sombra_ipe/settings.py
+++ sombra_ipe/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.onrender.com').split(',')
 SITE_ID = 1
 
 
